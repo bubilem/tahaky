97b96fe49471bb5c50ca2478ee551a3c3ca42b40
bubilem <mik.bubilek@gmail.com>
2021-02-15 18:02:22 +0100

    c-basics

--- work/GraphApp/src/graphapp/GraphApp.java
+++ work/GraphApp/src/graphapp/GraphApp.java
@@ -4,13 +4,20 @@ public class GraphApp {
 
     public static void main(String[] args) {
 
-        RowGraph.configure('#', 50);
-        RowGraph rg = new RowGraph("Ovoce 2019");
-        rg.add(new IntItem("jablka", 200));
-        rg.add(new IntItem("hrusky", 20));
-        rg.add(new IntItem("melouny", 150));
-        rg.add(new StringItem("víno", "150 kg"));
+        RowGraph.configure('#', 30);
+
+        RowGraph rg = new RowGraph("Ovoce 2020");
+        rg.add(new IntItem("jablka", 2));
+        rg.add(new IntItem("hrusky", 5));
+        rg.add(new IntItem("melouny", 20));
+        //rg.add(new StringItem("víno", "150 kg"));
         rg.print();
+
+        RowGraph g = new RowGraph("Auta 2020");
+        g.add(new IntItem("audi", 200));
+        g.add(new IntItem("skoda", 523));
+        g.add(new IntItem("vw", 74));
+        g.print();
 
     }
 
